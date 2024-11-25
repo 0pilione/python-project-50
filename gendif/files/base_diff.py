6aa28c7de23484eb0d51fe2db01663e6ce76cb26
Татьяна <deepredoctober@gmail.com>
2024-11-25 20:56:01 +0300

    ???

--- gendif/files/base_diff.py
+++ gendif/files/base_diff.py
@@ -1,45 +1,26 @@
 def diff(d1, d2):
-    if type(d1) == dict and type(d2) == dict:
-        keys1 = d1.keys()
-        keys2 = d2.keys()
-        unique_keys = set()
-        unique_keys.update(keys1)
-        unique_keys.update(keys2)
-        all_keys = list(unique_keys)
-        all_keys.sort()
-        d = {}
+    keys1 = d1.keys()
+    keys2 = d2.keys()
+    unique_keys = set()
+    unique_keys.update(keys1)
+    unique_keys.update(keys2)
+    all_keys = list(unique_keys)
+    all_keys.sort()
+    b = []
+    for key in all_keys:
         for key in all_keys:
-            if key in keys1 and key in keys2:
-                if d1[key] == d2[key]:
-                    d.update({'child': {
-                        'key': key, 
-                        'status': 'unupdated', 
-                        'value': d1[key],
-                        }})
-                if d1[key] != d2[key]:
-                    d.update({'child': {
-                        'key': key,
-                        'status': 'changed',
-                        'old_value': d1[key],
-                        'new_value': d2[key]}})
-                    if type(d1[key]) == dict and type(d2[key]) == dict:  
-                        d.update({'child': {
-                            'key': key,
-                            'status': 'nested',
-                            'value': diff(d1[key], d2[key])}})
-            if key in keys1 and key not in keys2:
-                d.update({'child': {
-                    'key': key,
-                    'status': 'deleted',
-                    'value': d1[key]}})
-            if key not in keys1 and key in keys2: 
-                if type(d2[key]) != dict:
-                    d.update({'child': {
-                              'key': key,
-                              'status': 'added',
-                              'value': d2[key]}})
+            if type(key) == dict:
+                if key in keys1 and key in keys2:
+                    if d1[key] == d2[key]:
+                        b.append({'key': key, 'status': 'unupdated', 'value':d1[key]})
+                        if d1[key] != d2[key]:
+                            b.append({'key': key, 'status': 'changed', 'old_value': d1[key], 'new_value': d2[key]})
+                            if type(d1[key]) == dict and type(d2[key]) == dict:  
+                                b.append({'key': key, 'status': 'nested', 'value': diff(d1[key], d2[key])})
+                        
+            if key in keys1 and key not in keys2:   
+                b.append({'key': key, 'status': 'deleted', 'value': d1[key]})
 
-                    
-
-    
-        return d
+            if key not in keys1 and key in keys2:   
+                b.append({'key': key, 'status': 'added', 'value': d2[key]})
+    return b
--- gendif/files/formater_stylish.py
+++ gendif/files/formater_stylish.py
@@ -1,19 +1,27 @@
 def stylish(diff, level=0):
     result = '{\n'
-    if type(diff) == dict:
-        if diff['child']['status'] == 'added':
-            result += f'{"    " * level + " + "}{diff["child"]["key"]}: {diff["child"]["value"]}'
-        if diff['child']['status'] == 'unupdated':
-            result += f'   {"    " * level}{diff["child"]["key"]}: {diff["child"]["value"]}'
-        if diff['child']['status'] == 'deleted':
-            result += f'{"    " * level + " - "}{diff["child"]["key"]}: {diff["child"]["value"]}'
-        if diff['child']['status'] == 'changed':
-            result += f'''{"    " * level + " - "}{diff["child"]["key"]}: {diff["child"]["old_value"]}\n
-            {"    " * level + " + "}{diff["child"]["key"]}: {diff["child"]["new_value"]}'''
-        if diff['child']['status'] == 'nested':
-            result += f'   {"    " * level}{diff["child"]["key"]}: {"{"}'
+    for n in diff:
+        if n['status'] == 'added':
+            result += f'{"    " * level + " + "}{n["key"]}: {n["value"]}\n'
+         
+        if n['status'] == 'unupdated':
+            
+            result += f'   {"    " * level}{n["key"]}: {n["value"]}\n'
+        if n['status'] == 'deleted':
+           
+            result += f'{"    " * level + " - "}{n["key"]}: {n["value"]}\n'
+        if n['status'] == 'changed':
+            
+           
+            result += f'''{"    " * level + " - "}{n["key"]}: {n["old_value"]}\n
+            {"    " * level + " + "}{n["key"]}: {n["new_value"]}\n'''
+        if n['status'] == 'nested':
+            
+            result += f'   {"    " * level}{n["key"]}: {"{"}\n'
             level += 1
-            stylish(diff['child']['value'], level)
-            result += f'{"    " * level}{"}"}'
+            stylish(n['value'], level)
+            
+            result += f'{"    " * level}{"}"}\n'
             level -= 1
+    result += '\n}'
     return result
