27ba16862b44fc0d17035b78677255a2ca17c420
Татьяна <deepredoctober@gmail.com>
2024-12-07 16:01:47 +0300

    final2

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-https://asciinema.org/a/ai1ZIUQRN6E7lVkbCeey59Sdk
+1. https://asciinema.org/a/a14oJSXYNx02c6RnJfTH7NHo5
+2. https://asciinema.org/a/RrxTHyaqYijY9l7DAvUASFjfP
+3. https://asciinema.org/a/LloGECisaUPI6bjU0IzNFzb44
+4. https://asciinema.org/a/40YcX1JrXFfKn1chNCogLtmlo
+5. https://asciinema.org/a/vFIFyFzJwxnFHcEiEBrCNknQc
 [![Maintainability](https://api.codeclimate.com/v1/badges/c113b0274a8a7ecb3483/maintainability)](https://codeclimate.com/github/0pilione/python-project-50/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/c113b0274a8a7ecb3483/test_coverage)](https://codeclimate.com/github/0pilione/python-project-50/test_coverage)
 [![Python CI](https://github.com/0pilione/python-project-50/actions/workflows/pyci.yml/badge.svg)](https://github.com/0pilione/python-project-50/actions/workflows/pyci.yml)
-
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
--- gendif/scripts/gendiff.py
+++ gendif/scripts/gendiff.py
@@ -1,23 +1,32 @@
 import argparse
-from  gendif.files.twofiles import generate_diff
 import json
+
+import yaml
+from gendif.scripts.twofiles import generate_diff
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
    
-m = parser.add_argument('first_file')
-n = parser.add_argument('second_file')
-parser.add_argument('--format', '-f', help='set format of output')
+first = parser.add_argument('first_file')
+second = parser.add_argument('second_file')
+formater = parser.add_argument('--format', '-f', help='set format of output', default="stylish")
 
 args = parser.parse_args()
 
-m = args.first_file
-n = args.second_file
+first = args.first_file
+second = args.second_file
+formater = args.format
 
-g = dict(json.load(open(m)))
-d = dict(json.load(open(n)))
-
-def main():
-   return  generate_diff(g, d)
+def main(): 
+   if first.endswith('json') and second.endswith('json'):
+      opened_first = dict(json.load(open(first)))
+      opened_second = dict(json.load(open(second)))
+      return  generate_diff(opened_first, opened_second, formater)
+   if first.endswith('yml') and second.endswith('yml'):
+      opened_first_2 = dict(yaml.safe_load(open(first)))
+      opened_second_2 = dict(yaml.safe_load(open(second)))
+      return  generate_diff(opened_first_2, opened_second_2, formater)
+   else:
+      return 'uncorrect type of files'
 
 
 if __name__ == '__main__':
