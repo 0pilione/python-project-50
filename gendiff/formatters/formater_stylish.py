7c751e03d3955479dc978608af3ae1df68e8e0df
Татьяна <deepredoctober@gmail.com>
2024-12-10 17:38:30 +0300

    &

--- gendiff/formatters/formater_stylish.py
+++ gendiff/formatters/formater_stylish.py
@@ -16,7 +16,7 @@ def stylish(diff, level=0):
             r.append(f'\n{"    " * level}{n["key"]}:')
             r.append(f'{stylish(n["value"], level)}')
             level -= 1
-    r.append(f'\n{"    " * level} {"}"}')
+    r.append(f'\n{"    " * level}{"}"}')
     b = ''.join(r)
     level -= 1
     return ' {' + b
