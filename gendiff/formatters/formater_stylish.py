4d73e03dc3e539973308a3a95484aaac9b66785c
Татьяна <deepredoctober@gmail.com>
2024-12-10 17:02:58 +0300

    &

--- gendiff/formatters/formater_stylish.py
+++ gendiff/formatters/formater_stylish.py
@@ -3,20 +3,20 @@ def stylish(diff, level=0):
     r = []
     for n in diff:
         if n['status'] == 'added':
-           r.append(f'\n{"    " * level + "  + "}{n["key"]}: {stylish_2(n["value"], level)}')
+           r.append(f'\n{"    " * level + "  + "}{n["key"]}: {stylish_2(n["value"], level)} ')
         if n['status'] == 'unupdated':
-            r.append(f'\n{"    " * level + "    "}{n["key"]}: {stylish_2(n["value"], level)}')
+            r.append(f'\n{"    " * level + "    "}{n["key"]}: {stylish_2(n["value"], level)} ')
         if n['status'] == 'deleted':
-            r.append(f'\n{"    " * level + "  - "}{n["key"]}: {stylish_2(n["value"], level)}')
+            r.append(f'\n{"    " * level + "  - "}{n["key"]}: {stylish_2(n["value"], level)} ')
         if n['status'] == 'changed':
-            r.append(f'\n{"    " * level + "  - "}{n["key"]}: {stylish_2(n["old_value"], level)}')
-            r.append(f'\n{"    " * level + "  + "}{n["key"]}: {stylish_2(n["new_value"], level)}')
+            r.append(f'\n{"    " * level + "  - "}{n["key"]}: {stylish_2(n["old_value"], level)} ')
+            r.append(f'\n{"    " * level + "  + "}{n["key"]}: {stylish_2(n["new_value"], level)} ')
         if n['status'] == 'nested':
             level += 1
-            r.append(f'\n{"    " * level}{n["key"]}:')
-            r.append(f'{stylish(n["value"], level)}')
+            r.append(f'\n{"    " * level}{n["key"]}: ')
+            r.append(f'{stylish(n["value"], level)} ')
             level -= 1
-    r.append(f'\n{"    " * level}{"}"}')
+    r.append(f'\n{"    " * level}{"}"} ')
     b = ' '.join(r)
     level -= 1
     return '{' + b
